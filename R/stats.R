0baf14b17b8fe448422c21897e6276bc1b15d1e0
Jerry Zhu <jerryzhujian9@gmail.com>
2016-01-29 22:47:59 -0500

    view

--- R/stats.R
+++ R/stats.R
@@ -20,3 +20,13 @@ z.show = function(...){
 #' @examples
 #' @export
 z.info = z.show
+
+#' show information about a data frame or similar object (like spss variable view)
+#' @description wrapper of \code{\link[sjPlot]{view_df}}; can make the html bigger by openning in internet browser
+#' @param
+#' @return
+#' @examples
+#' @export
+z.view = function(x, showFreq = T, showPerc = T, sortByName = F, ...){
+    sjPlot::view_df(x, showFreq = showFreq, showPerc = showPerc, sortByName = sortByName, ...)
+}
