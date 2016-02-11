40785d62c5c6eec6d24d1bbd949d61a489792043
Jerry Zhu <jerryzhujian9@gmail.com>
2016-02-11 02:34:12 -0500

    plot

--- R/plot.R
+++ R/plot.R
@@ -8,12 +8,12 @@
 #' first z.export(filename) to create a new canvas
 #' then plot all following graphs to it and
 #' finally dev.off()/z.clean(3) to unlink
-
+#'
 #' pdf(file = ifelse(onefile, "Rplots.pdf", "Rplot%03d.pdf"),
 #' width, height, onefile, family, title, fonts, version,
 #' paper, encoding, bg, fg, pointsize, pagecentre, colormodel,
 #' useDingbats, useKerning, fillOddEven, compress)
-
+#'
 #' i = 100
 #' fileName = z.join('plot', i, '.pdf')
 #' @export
@@ -41,15 +41,15 @@ z.subplot = function(n, m, ...){
 #' # e.g.,
 #' # 3 rows subplots
 #' par(mfrow=c(3,1) )
-
+#'
 #' plot(1:10, 1:10, main = "Plot 1")
 #' # draw another arbitrary subplot
 #' z.embed(plot(10,10, xlab="", ylab=""), x=2, y=8, size = c(0.5, 0.5) )
-
+#'
 #' # demonstrates mfg usage, draw the third plot before the drawing the second
 #' par(mfg=c(3,1) ); plot(11:20, 11:20, main = "Plot 3")
 #' z.embed(plot(10,10, xlab="", ylab=""), x=12, y=18, size = c(0.5, 0.5))
-
+#'
 #' par(mfg=c(2,1)); plot(21:30, 21:30, main = "Plot 2")
 #' z.embed(plot(10,10, xlab="", ylab=""), x=22, y=28, size = c(0.5, 0.5))
 #' @export
@@ -108,4 +108,92 @@ z.embed = function(fun, x, y=NULL, size=c(1,1), vadj=0.5, hadj=0.5,
     tmp.par <- par(no.readonly=TRUE)
 
     return(invisible(tmp.par))
+}
+
+#' plot a customized boxplot with jittered stripplot, violin, and mean
+#' @param df data frame in long format
+#' @param cmd like "y|x z" or "y|x" where y is continous, x z are discrete
+#' @return a ggplot object (+theme_apa() to get apa format plot)
+#' @examples
+#' @export
+z.describe = function(df,cmd){
+    # yy|xx zz
+    cmd = strsplit(cmd,"|",fixed=TRUE)[[1]]
+    yy = cmd[1]
+    xx = strsplit(cmd[2]," ",fixed=TRUE)[[1]]
+    if (length(xx)==1) {
+        xx = xx[1]
+        tt = sprintf('
+                    pp = ggplot2::ggplot(df, aes(x=%s, y=%s)) +
+                        geom_violin() +
+                        geom_boxplot(outlier.shape=NA, alpha=0.7) + # avoid plotting outliers twice from geom_jitter
+                        geom_point(position=position_jitter(width=0.2, height=0), size=1) +
+                        stat_summary(fun.y=mean, color="darkred", geom="point", shape=18, size=3) +
+                        theme(legend.position="none")'
+                     , xx, yy
+        )
+        tt = paste0(tt, ' + stat_summary(fun.y=mean, color="darkred", geom="text",vjust=-0.7, aes(label=sprintf("%.2f", ..y..)), alpha=1) # ..y.. internal variable computed mean')
+    } else {
+        zz = xx[2]
+        xx = xx[1]
+        tt = sprintf('
+                     pp = ggplot2::ggplot(df, aes(x=%s, y=%s, color=%s)) +
+                        geom_violin() +
+                        geom_boxplot(outlier.shape=NA, alpha=0.7) + # avoid plotting outliers twice from geom_jitter
+                        geom_point(position=position_jitter(width=0.2, height=0), size=1) +
+                        stat_summary(fun.y=mean, color="darkred", geom="point", shape=18, size=3) +
+                        facet_grid(~%s) +
+                        theme(legend.position="none")'
+                     , xx, yy, zz, zz
+        )
+        tt = paste0(tt, ' + stat_summary(fun.y=mean, color="darkred", geom="text",vjust=-0.7, aes(label=sprintf("%.2f", ..y..)), alpha=1) # ..y.. internal variable computed mean')
+    }
+    eval(parse(text = tt))
+    return(pp)
+}
+
+#' plot a heatmap with values shown
+#' @param df data frame in wide format, like a correlation matrix
+#' @param id a column name as id, will be shown as x axis, quoted ""
+#' @param show.values whether to show values, default TRUE
+#' @return a ggplot object (+theme_apa() to get apa format plot)
+#' @examples
+#' @export
+z.heatmap = function(df, id, show.values=T){
+    cmd = sprintf('tidyr::gather(df, key,value,-%s,factor_key = T) -> df
+                  df$%s = factor(df$%s,rev(unique(as.character(df$%s))))
+                  ',id,id,id,id)
+    eval(parse(text = cmd))
+
+    x = "key"; y = id; z = "value"
+    if (show.values) {
+        t = sprintf('
+                    p = ggplot(df, aes(%s, %s)) +
+                    geom_tile(aes(fill = %s)) +
+                    scale_fill_gradient2(low = "blue", mid = "white", high = "red") +
+                    geom_text(aes(fill = %s, label = round(%s, 1))) +
+                    scale_x_discrete("", expand = c(0, 0)) +
+                    scale_y_discrete("", expand = c(0, 0)) +
+                    theme_grey(base_size = 9) +
+                    theme(legend.position = "right",
+                    axis.ticks = element_blank(),
+                    axis.text.x = element_text(angle = 330, hjust = 0))'
+                    , x, y, z, z, z
+)
+    } else {
+        t = sprintf('
+                    p = ggplot(df, aes(%s, %s)) +
+                    geom_tile(aes(fill = %s)) +
+                    scale_fill_gradient2(low = "blue", mid = "white", high = "red") +
+                    scale_x_discrete("", expand = c(0, 0)) +
+                    scale_y_discrete("", expand = c(0, 0)) +
+                    theme_grey(base_size = 9) +
+                    theme(legend.position = "right",
+                    axis.ticks = element_blank(),
+                    axis.text.x = element_text(angle = 330, hjust = 0))'
+                    , x, y, z
+        )
+    }
+    eval(parse(text = t))
+    return(p)
 }
