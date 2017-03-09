a46d75c4203b1023b94f0e6a5ff4b73845be2dde
Jerry Zhu <jerryzhujian9@gmail.com>
2017-03-09 16:50:33 -0500

    update

--- R/basic.R
+++ R/basic.R
@@ -367,8 +367,8 @@ ez.print = function(...,sep=''){
 #' @examples
 #' ez.eval('z=9',environment())
 #' @export
-ez.eval = function(cmd){
-    eval(parse(text = cmd),envir = parent.frame())
+ez.eval = function(cmd,env=parent.frame()){
+    eval(parse(text = cmd),envir = env)
 }
 
 #' wrapper of \code{\link{eval}}
