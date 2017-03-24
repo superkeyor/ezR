75582d859b959b54471484cb5c85615d31c80e38
Jerry Zhu <jerryzhujian9@gmail.com>
2017-03-24 14:43:31 -0400

    update

--- R/plot.R
+++ R/plot.R
@@ -451,7 +451,7 @@ ez.describe = function(df,cmd,violin=TRUE){
                 xx = xx[1]
                 tt = sprintf('
                              fun_length <- function(x){return(data.frame(y=min(x),label= paste0(length(x)," (n)")))}  # http://stackoverflow.com/a/15720769/2292993
-                             pp = ggplot2::ggplot(df, aes(x=%s, y=%s, color=%s, fill=%s)) +
+                             pp = ggplot2::ggplot(df, aes(x=%s, y=%s, color=c(%s,%s), fill=c(%s,%s))) +
                              stat_boxplot(geom = "errorbar", width = 0.5) +
                              %s geom_boxplot(outlier.shape=NA) + # avoid plotting outliers twice from geom_jitter
                              geom_point(position=position_jitter(width=0.2, height=0), size=1) +
@@ -459,7 +459,7 @@ ez.describe = function(df,cmd,violin=TRUE){
                              facet_grid(~%s) +
                              theme(legend.position="none") +
                              ggtitle(paste0("N = ",nrow(df)))'
-                             , xx, yy, zz, zz, violin, zz
+                             , xx, yy, yy,zz, yy,zz, violin, zz
                 )
                 tt = paste0(tt, ' + \nstat_summary(fun.data = fun_length, color="royalblue", geom="text",vjust=1.2)')
                 tt = paste0(tt, ' + \nstat_summary(fun.y=mean, color="darkred", geom="text",vjust=-0.7, aes(label=sprintf("%.2f (M)", ..y..)), alpha=1) # ..y.. internal variable computed mean')
