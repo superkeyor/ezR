24a0cfcf8267b3b4175186d2d99e2821a1fdcf64
Jerry Zhu <jerryzhujian9@gmail.com>
2017-05-02 18:24:19 -0400

    anovas regressions

--- R/stats.R
+++ R/stats.R
@@ -51,3 +51,89 @@ ez.se = function(x) {
     # http://stackoverflow.com/a/7220087/2292993
     sqrt(var(x,na.rm=TRUE)/length(na.omit(x)))
 }
+
+#' a series of simple regression, for many y and many x
+#' @description lm(scale(df[[yy]])~scale(df[[xx]]))
+#' @param df a data frame, if its column is factor, auto converts to numeric (internally call ez.2value(df))
+#' @param y a vector of outcome variables c('var1','var2'), or a single variable 'var1'
+#' @param x a vector of predictors, or a single predictor, (eg, names(select(beta,Gender:dmce)))
+#' @param pthreshold default .05, print/output results whenever p < pthreshold, could be 1 then get all
+#' @param showerror whether show error message when error occurs, default F
+#' @return an invisible data frame with y,x,p,b and print results out on screen; results can then be saved using ez.savex(results,'results.xlsx')
+#' @examples
+#' @export
+ez.regressions = function(df,y,x,pthreshold=.05,showerror=F) {
+    results = ez.header('y'=character(),'x'=character(),'p'=numeric(),'b'=numeric())
+    for (yy in y) {
+        # note a new row needs to have the same column numbers defined in header
+        results = ez.append(results,c(yy,'',paste0('n = ',ez.size(df,1)),''))
+        for (xx in x) {
+            dfdf=ez.2value(df)
+            if (showerror) {
+                # try is implemented using tryCatch
+                # try(expr, silent = FALSE)
+                try({
+                    lm(scale(dfdf[[yy]])~scale(dfdf[[xx]])) %>% summary() ->model
+                    p = model$coefficients[2,4]
+                    b = model$coefficients[2,1]
+                    if (p < pthreshold) {results = ez.append(results,c(yy,xx,p,b))}
+                    })
+            } else {
+                # go to next loop item, in case error
+                tryCatch({
+                    lm(scale(dfdf[[yy]])~scale(dfdf[[xx]])) %>% summary() ->model
+                    p = model$coefficients[2,4]
+                    b = model$coefficients[2,1]
+                    if (p < pthreshold) {results = ez.append(results,c(yy,xx,p,b))}
+                }, error = function(e) {})
+            }
+        }
+        results = ez.append(results,c('','','',''))  # empty line between each y
+    }
+    return(invisible(results))
+}
+
+#' a series of one-way anova, for many y and many x
+#' @description aov(ez.2value(df[[yy]])~df[[xx]])
+#' @param df a data frame
+#' @param y a vector of continous variables c('var1','var2'), or a single variable 'var1', if it is a factor, auto converts to numeric (internally call ez.2value(df[[yy]]), (eg, names(select(beta,Gender:dmce)))
+#' @param x a vector of categorical variables, or a single categorical variable
+#' @param pthreshold default .05, print/output results whenever p < pthreshold, could be 1 then get all
+#' @param showerror whether show error message when error occurs, default F
+#' @return an invisible data frame with x,y,p,means and print results out on screen; results can then be saved using ez.savex(results,'results.xlsx')
+#' \cr the means column in excel can be split into mulitiple columns using Data >Text to Columns
+#' @examples
+#' @export
+ez.anovas = function(df,y,x,pthreshold=.05,showerror=F) {
+    results = ez.header('x'=character(),'y'=character(),'p'=numeric(),'means'=numeric())
+    for (xx in x) {
+        # note a new row needs to have the same column numbers defined in header
+        results = ez.append(results,c(xx,'',paste0('n = ',ez.size(df,1)),''))
+        for (yy in y) {
+            if (showerror) {
+                # try is implemented using tryCatch
+                # try(expr, silent = FALSE)
+                try({
+                    a = aov(ez.2value(df[[yy]])~df[[xx]])
+                    p = summary(a)[[1]][["Pr(>F)"]][[1]]
+                    s = aggregate(ez.2value(df[[yy]])~df[[xx]],FUN=mean)
+                    means = ''
+                    for (i in 1:ez.size(s,1)) {means = paste(means,s[i,1],s[i,2],sep='\t')}
+                    if (p < pthreshold) {results = ez.append(results,c(xx,yy,p,means))}
+                    })
+            } else {
+                # go to next loop item, in case error
+                tryCatch({
+                    a = aov(ez.2value(df[[yy]])~df[[xx]])
+                    p = summary(a)[[1]][["Pr(>F)"]][[1]]
+                    s = aggregate(ez.2value(df[[yy]])~df[[xx]],FUN=mean)
+                    means = ''
+                    for (i in 1:ez.size(s,1)) {means = paste(means,s[i,1],s[i,2],sep='\t')}
+                    if (p < pthreshold) {results = ez.append(results,c(xx,yy,p,means))}
+                }, error = function(e) {})
+            }
+        }
+        results = ez.append(results,c('','','',''))  # empty line between each x
+    }
+    return(invisible(results))
+}
