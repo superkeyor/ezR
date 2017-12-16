95f1ea2da04675497bfcd5bd06214c9be9462814
Jerry Zhu <jerryzhujian9@gmail.com>
2017-12-16 05:51:05 -0600

    update

--- R/plot.R
+++ R/plot.R
@@ -1597,7 +1597,7 @@ ez.scatterplot = function(df,cmd,rp.size=5,rp.x=0.95,rp.y=0.95,point.alpha=0.95,
 #' \cr ez.countplot(iris, 'Sepal.Length|Species',bar.width = 0.1,n.size = 3)
 #' @param df data frame in long format (but be careful that standard error might be inaccurate depending on grouping in the long format)
 #' @param cmd like "x, x|z, x|z a" where x z a are all discrete
-#' @param position so far can only be "stack" or "fill" ('dodge' not supported yet)
+#' @param position so far can only be 1 ("stack"), 2 ("fill"), 3 ("stack+fill"). ('dodge' not supported yet)
 #' @param n.size set to 0 to hide count/percentage
 #' @param alpha bar alpha value
 #' @param bar.color  "bw" or "color"  black/white or colorblind-friendly color
@@ -1619,7 +1619,18 @@ ez.scatterplot = function(df,cmd,rp.size=5,rp.x=0.95,rp.y=0.95,point.alpha=0.95,
 #' \cr see http://stackoverflow.com/a/31437048/2292993 for discussion
 #' @examples 
 #' @export
-ez.countplot = function(df,cmd,position='stack',bar.color='color',alpha=1,n.size=5.5,bar.width=0.7,ylab=NULL,xlab=NULL,zlab=NULL,legend.position='top',legend.direction="horizontal",legend.box=T,legend.size=c(0,10),xangle=0,vjust=NULL,hjust=NULL,print.cmd=FALSE) {
+ez.countplot = function(df,cmd,position=3,bar.color='color',alpha=1,n.size=5.5,bar.width=0.7,ylab=NULL,xlab=NULL,zlab=NULL,legend.position='top',legend.direction="horizontal",legend.box=T,legend.size=c(0,10),xangle=0,vjust=NULL,hjust=NULL,print.cmd=FALSE) {
+    # http://stackoverflow.com/a/25734388/2292993
+    # Merge Multiple spaces to single space, and remove trailing/leading spaces 
+    # also see trimws()--remove trailing/leading spaces
+    cmd = gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "", cmd, perl=TRUE)
+    cmd = strsplit(cmd,"|",fixed=TRUE)[[1]]
+    if (length(cmd)==1 & position==3) position='stack'
+    if (position==3) {
+        p1=ez.countplot(df,cmd,'stack',bar.color, alpha, n.size, bar.width, ylab, xlab, zlab, legend.position, legend.direction, legend.box, legend.size, xangle, vjust, hjust, print.cmd)
+        p1=ez.countplot(df,cmd,'fill',bar.color, alpha, n.size, bar.width, ylab, xlab, zlab, legend.position, legend.direction, legend.box, legend.size, xangle, vjust, hjust, print.cmd)
+        return(invisible(ggmultiplot(p1,p2,cols=2)))
+    }
     
     # https://stackoverflow.com/a/25215323/2292993
     # call options(warn=1) to set the global warn (opt is alway global, even change inside a function) to 1, but returns the old value to oldWarn
@@ -1635,11 +1646,6 @@ ez.countplot = function(df,cmd,position='stack',bar.color='color',alpha=1,n.size
     legend.box = ifelse(legend.box,'theme(legend.background = element_rect(color = "black"))+','')
     vjust = ifelse(is.null(vjust),'',sprintf(',vjust=%f',vjust))
     hjust = ifelse(is.null(hjust),'',sprintf(',hjust=%f',hjust))
-    # http://stackoverflow.com/a/25734388/2292993
-    # Merge Multiple spaces to single space, and remove trailing/leading spaces 
-    # also see trimws()--remove trailing/leading spaces
-    cmd = gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "", cmd, perl=TRUE)
-    cmd = strsplit(cmd,"|",fixed=TRUE)[[1]]
     # xx
     if (length(cmd)==1) {
         if (bar.width<0.9) bar.width=0.98
