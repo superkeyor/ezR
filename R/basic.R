38d094bbf46da80f7d421e6f1a19f9fb0de6914a
Jerry Zhu <jerryzhujian9@gmail.com>
2018-05-17 16:07:06 -0500

    is.empty

--- R/basic.R
+++ R/basic.R
@@ -23,6 +23,155 @@ and = magrittr::and
 #' @export
 or = magrittr::or
 
+#' Empty Value
+#'
+#' Rails-inspired helper that checks if vector values are "empty", i.e. if it's: \code{NULL}, zero-length, \code{NA}, \code{NaN}, \code{FALSE}, an empty string or \code{0}. Note that unlike its native R \code{is.<something>} sibling functions, \code{is.empty} is vectorised (hence the "values").
+#' @param x an object to check its emptiness
+#' @param trim trim whitespace? (\code{TRUE} by default)
+#' @param ... additional arguments for \code{\link{sapply}}
+#' @examples \dontrun{
+#' is.empty(NULL)     # [1] TRUE
+#' is.empty(c())      # [1] TRUE
+#' is.empty(NA)       # [1] TRUE
+#' is.empty(NaN)      # [1] TRUE
+#' is.empty("")       # [1] TRUE
+#' is.empty(0)        # [1] TRUE
+#' is.empty(0.00)     # [1] TRUE
+#' is.empty("    ")   # [1] TRUE
+#' is.empty("foobar") # [1] FALSE
+#' is.empty("    ", trim = FALSE)    # [1] FALSE
+#' # is.empty is vectorised!
+#' all(is.empty(rep("", 10)))        # [1] TRUE
+#' all(is.empty(matrix(NA, 10, 10))) # [1] TRUE
+#' }
+#' @export
+#' @note copied from https://cran.r-project.org/web/packages/rapportools/index.html
+ez.is.empty <- function(x, trim = TRUE, ...) {
+    if (length(x) <= 1) {
+        if (is.null(x))
+            return (TRUE)
+        if (length(x) == 0)
+            return (TRUE)
+        if (is.na(x) || is.nan(x))
+            return (TRUE)
+        if (is.character(x) && nchar(ifelse(trim, trim.space(x), x)) == 0)
+            return (TRUE)
+        if (is.logical(x) && !isTRUE(x))
+            return (TRUE)
+        if (is.numeric(x) && x == 0)
+            return (TRUE)
+        return (FALSE)
+    } else
+        sapply(x, ez.is.empty, trim = trim, ...)
+}
+
+#' Strings
+#'
+#' Checks if provided object is a string i.e. a length-one character vector.
+#' @param x an object to check
+#' @return a logical value indicating whether provided object is a string
+#' @examples
+#'     is.string("foobar")          # [1] TRUE
+#'     is.string(1)                 # [1] FALSE
+#'     is.string(c("foo", "bar"))   # [1] FALSE
+#' @export
+#' @note copied from https://cran.r-project.org/web/packages/rapportools/index.html
+ez.is.string <- function(x){
+    is.character(x) && length(x) == 1
+}
+
+#' Boolean
+#'
+#' Checks if provided object is a boolean i.e. a length-one logical vector.
+#' @param x an object to check
+#' @return a logical value indicating whether provided object is a boolean
+#' @examples \dontrun{
+#'     is.boolean(TRUE)                # [1] TRUE
+#'     # the following will work on most systems, unless you have tweaked global Rprofile
+#'     is.boolean(T)                   # [1] TRUE
+#'     is.boolean(1)                   # [1] FALSE
+#'     is.string(c("foo", "bar"))      # [1] FALSE
+#' }
+#' @export
+#' @note copied from https://cran.r-project.org/web/packages/rapportools/index.html
+ez.is.boolean <- function(x){
+    is.logical(x) && length(x) == 1
+}
+
+#' Numbers
+#'
+#' Checks if provided object is a number, i.e. a length-one numeric vector.
+#' @param x an object to check
+#' @param integer logical: check if number is integer
+#' @return a logical value indicating whether provided object is a string
+#' @examples
+#' is.number(3)              # [1] TRUE
+#' is.number(3:4)            # [1] FALSE
+#' is.number("3")            # [1] FALSE
+#' is.number(NaN)            # [1] TRUE
+#' is.number(NA_integer_)    # [1] TRUE
+#' @export
+#' @note copied from https://cran.r-project.org/web/packages/rapportools/index.html
+ez.is.number <- function(x, integer = FALSE) {
+    check.fn <- if (isTRUE(integer)) is.integer else is.numeric
+    do.call(check.fn, list(x)) && length(x) == 1
+}
+
+#' Variables
+#'
+#' From \emph{rapport}'s point of view, a \code{variable} is a non-\code{NULL} atomic vector that has no dimension attribute (see \code{dim} for details). This approach bypasses \code{factor} issues with \code{\link{is.vector}}, and also eliminates multidimensional vectors, such as matrices and arrays.
+#' @param x an object to be checked for "variable" format
+#' @return a logical value indicating that provided object is a "variable"
+#' @examples
+#' is.variable(rnorm(100))  # [1] TRUE
+#' is.variable(LETTERS)     # [1] TRUE
+#' is.variable(NULL)        # [1] FALSE
+#' is.variable(mtcars)      # [1] FALSE
+#' is.variable(HairEyeColor[, , 1])  # [1] FALSE
+#' is.variable(list())      # [1] FALSE
+#' @export
+#' @note copied from https://cran.r-project.org/web/packages/rapportools/index.html
+ez.is.variable <- function(x){
+    if (base::missing(x))
+        stop('test object not provided')
+    is.atomic(x) & !is.null(x) & is.null(dim(x))
+}
+
+#' Tabular Structure
+#'
+#' Checks if object has "tabular" structure (not to confuse with \code{\link{table}}) - in this particular case, that means \code{\link{matrix}} and \code{\link{data.frame}} objects only.
+#' @param x an object to be checked for "tabular" format
+#' @return a logical value indicating that provided object has tabular structure
+#' @examples
+#' is.tabular(HairEyeColor[, , 1])  # [1] TRUE
+#' is.tabular(mtcars)               # [1] TRUE
+#' is.tabular(table(mtcars$cyl))    # [1] FALSE
+#' is.tabular(rnorm(100))           # [1] FALSE
+#' is.tabular(LETTERS)              # [1] FALSE
+#' is.tabular(pi)                   # [1] FALSE
+#' @export
+#' @note copied from https://cran.r-project.org/web/packages/rapportools/index.html
+ez.is.tabular <- function(x){
+    if (base::missing(x))
+        stop('no object to test table')
+    inherits(x, c('matrix', 'data.frame')) && length(dim(x)) == 2
+}
+
+#' Check integers
+#'
+#' This function tests if given variable "appears" to be an integer. To qualify as such, two conditions need to be satisfied: it should be stored as \code{\link{numeric}} object, and it should pass regular expression test if it consists only of digits.
+#' @param x a numeric variable that is to be tested
+#' @return a logical value that indicates that tested variable "looks like" integer
+#' @export
+#' @note copied from https://cran.r-project.org/web/packages/rapportools/index.html
+ez.is.integer.like <- function(x){
+
+    if (missing(x))
+        stop('no object to test integer')
+
+    is.numeric(x) & all(grepl('^-?[[:digit:]]+$', x))
+}
+
 #' alias of \code{\link{class}}
 #' @description alias of \code{\link{class}}
 #' @export
