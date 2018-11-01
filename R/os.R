efe2d7238c95a170b52e960eccee41f0c9d31c5f
Jerry Zhu <jerryzhujian9@gmail.com>
2018-11-01 18:32:22 -0400

    update

--- R/os.R
+++ R/os.R
@@ -307,7 +307,7 @@ ez.execute = system
 ez.joinpath = function(...) {
     sep = .Platform$file.sep
     result = gsub(paste0(sep,"{2,}"), sep, file.path(...), fixed=FALSE, perl=TRUE)
-    result = gsub(paste0(sep,"$"), '', file.path(...), fixed=FALSE, perl=TRUE)
+    result = gsub(paste0(sep,"$"), '', result, fixed=FALSE, perl=TRUE)
     return(result)
 }
 
