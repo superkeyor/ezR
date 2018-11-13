ef1936514bb3bea52217cb1498c580f4707962b5
Jerry Zhu <jerryzhujian9@gmail.com>
2018-11-13 11:53:50 -0500

    update

--- R/stats.R
+++ R/stats.R
@@ -401,11 +401,11 @@ ez.vi = function(x,printn=35,printcn=600,order='as') {
             cat(sprintf('Counts/Levels (Incl NA): \n%s\n',v.levels %>% toString(width=printcn)))
         }
         cat(sprintf("\nUniques (Incl NA, NA might be printed as 'NA'): \n%s\n", v.elements))
-        cat(sprintf('#Unique (Incl NA): %d\t#NA: %d (%.0f%%)\t#Non-NA: %d\t#Total: %d\n', v.unique, v.missing, v.missing*100/v.n, v.n-v.missing, v.n))
+        cat(sprintf('\n#Unique (Incl NA): %d\t#NA: %d (%.0f%%)\t#Non-NA: %d\t#Total: %d\n', v.unique, v.missing, v.missing*100/v.n, v.n-v.missing, v.n))
         if ( (is.numeric(v) | is.date(v)) & !all(is.na(v)) ) {
             cat(sprintf('M = %.2f\tSD = %.2f\tRange = (%.2f,%.2f)\tSum = %.2f\n', v.mean, v.sd, v.min, v.max, v.sum))
         }
-        cat(sprintf('\n%-25s',v.class))
+        cat(sprintf('\n%-25s\n',v.class))
         cat(sprintf('attributes: %s\n',v.attrs))
     }
     return(invisible(NULL))
