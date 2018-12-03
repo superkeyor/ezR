02078fe7cddd3925fa389f5e6af90419b7a2b429
Jerry Zhu <jerryzhujian9@gmail.com>
2018-12-03 10:53:24 -0500

    update

--- R/frame.R
+++ R/frame.R
@@ -557,13 +557,8 @@ ez.factorelevel = function(x, cols=NULL, print2screen=F) {
             labs = ez.getlabels(x)
 
             # a bit ugly. 
-            actual = unique(as.character(x))
-            ord = match( actual, levels(x), nomatch=0 )
-            # in case many levels, so 1, 10, 100 order weirdly 
-            ord = paste0('ord',sprintf('%36d',ord))
-            names(actual) = ord
-            ord = actual[sort(ord)]
-            ord = unname(ord)
+            # in case many levels, where 1, 10, 100 order weirdly 
+            actual = unique(as.character(x)); ord = match( actual, levels(x), nomatch=0 ); ord = paste0('ord',sprintf('%036d',ord)); names(actual) = ord; ord = actual[sort(ord)]; ord = unname(ord)
 
             x = factor(x, ord)
             attr(x,'label') <- varlab
@@ -935,6 +930,8 @@ ez.replace = function(df, col, oldval, newval=NULL, print2screen=T){
             lab = ez.getlabel(dfcol); labs = ez.getlabels(dfcol)
             # for factor, you cannot directly assign if the new val is not alredy in the levels, otherwise get "invalid factor level, NA generated"
             factored = ifelse(is.factor(dfcol), TRUE, FALSE)
+            x=dfcol
+            actual = unique(as.character(x)); ord = match( actual, levels(x), nomatch=0 ); ord = paste0('ord',sprintf('%036d',ord)); names(actual) = ord; ord = actual[sort(ord)]; ord = unname(ord)
             if (is.factor(dfcol)) {dfcol=as.character(dfcol)}
 
             if (is.na(oldval)) {
@@ -949,7 +946,7 @@ ez.replace = function(df, col, oldval, newval=NULL, print2screen=T){
                 }
             }
 
-            if (factored) {dfcol=as.factor(dfcol)}
+            if (factored) {dfcol=factor(dfcol,ord)}
             dfcol = ez.setlabel(dfcol,lab); dfcol = ez.setlabels(dfcol,ez.flipflop(labs))
             return(dfcol)
         }
@@ -1029,10 +1026,12 @@ ez.replacewhen = function(df, print2screen=T, ...) {
 
                 lab = ez.getlabel(df[[col]]); labs = ez.getlabels(df[[col]])
                 factored = ifelse(is.factor(df[[col]]), TRUE, FALSE)
+                x = df[[col]]
+                actual = unique(as.character(x)); ord = match( actual, levels(x), nomatch=0 ); ord = paste0('ord',sprintf('%036d',ord)); names(actual) = ord; ord = actual[sort(ord)]; ord = unname(ord)
                 if (is.factor(df[[col]])) {df[[col]]=as.character(df[[col]])}
                 # df[theRow,col]=newval  # this syntax works also, but df[145:146,2,drop=F]=4 says unused arg drop=F
                 df[[col]][theRow2] <- newval
-                if (factored) {df[[col]]=as.factor(df[[col]])}
+                if (factored) {df[[col]]=factor(df[[col]],ord)}
                 df[[col]] = ez.setlabel(df[[col]],lab); df[[col]] = ez.setlabels(df[[col]],ez.flipflop(labs))
             }
         } # end for
