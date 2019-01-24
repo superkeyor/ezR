a1622c1ed796aca6018b23592d1f884a46e1ab80
Jerry Zhu <jerryzhujian9@gmail.com>
2019-01-24 17:38:57 -0500

    update

--- R/plot.R
+++ R/plot.R
@@ -2388,6 +2388,7 @@ ez.scatterplot = function(df,cmd,loess=FALSE,model=c('lm', 'lmrob', 'lmRob', 'rl
         # a bit ugly hack
         p1strip = paste0(p2$condlevels[[1]],collapse='+')
         pp = ez.esp('latticeExtra:::c.trellis("{p1strip}"=p1,p2,y.same=TRUE,layout=c(dim(p1)+dim(p2),1))') 
+        if (!missing(layout)) pp$layout = layout
         return(pp)
     }
 
