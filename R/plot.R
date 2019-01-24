8deb2a69c4533c3961fecd183cfc59f4e446636a
Jerry Zhu <jerryzhujian9@gmail.com>
2019-01-24 12:20:11 -0500

    update

--- R/plot.R
+++ R/plot.R
@@ -189,7 +189,7 @@ ez.palette = function(name, n, type = c("discrete", "continuous"), nn=10){
         Accent = c('#7FC97F', '#BEAED4', '#FDC086', '#FFFF99', '#386CB0', '#F0027F', '#BF5B17', '#666666'),
         Set1Remix = c("#984EA3","#377EB8","#4DAF4A","#FF7F00","#E41A1C","#FFFF33","#A65628","#F781BF"),
         # Jerry remixed from Set 3
-        Zhu = c("#B3DE69","#80B1D3","#BC80BD","#FFED6F","#FB8072"),
+        Zhu = c("#B3DE69","#80B1D3","#BC80BD","#FB8072","#FDB462"),
         # well, I tested with col2grey(), not so true. Not sure about actual bw printer
         Printer = c("#FDAE61","#2B83BA","#D7191C","#ABDDA4","#FFFFBF"),
         Colorblind = c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00","#CC79A7","#000000")
