7700c7533a8640c93d23d99e32f5769a3a3b9b31
Jerry Zhu <jerryzhujian9@gmail.com>
2019-02-27 16:30:45 -0500

    update

--- R/stats.R
+++ R/stats.R
@@ -371,7 +371,7 @@ ez.vv = function(vec,printn=Inf,order='as',quote=NULL,print2scr=FALSE){
 #' @examples
 #' \dontrun{
 #' # sprintf("c(%s)",ez.vv(vec,...)). 
-#' # ez.sprintf('model = "{model}", type = {ez.vc(type)}')
+#' # ez.sprintf('model = "\{model\}", type = \{ez.vc(type)\}')
 #' }
 #' @export
 ez.vc = function(vec,...){
