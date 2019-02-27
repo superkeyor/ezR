c0d356154eb2ef48bc4dd609298d74ae704fcec8
Jerry Zhu <jerryzhujian9@gmail.com>
2019-02-27 18:37:51 -0500

    update

--- R/plot.R
+++ R/plot.R
@@ -2223,8 +2223,8 @@ ez.wherena = function(df,id=NULL,color="red",angle=270,basesize=9,xsize=1,ysize=
 #' \cr jitter.x=T, jitter.y=T, factor=1, amount=NULL see \code{\link[base]{jitter}}
 #' \cr layout=c(col,row)--a bit weird. lattice orders panels from the bottom up. 
 #' To get what you want either use as.table = TRUE or reverse the ordering in index.cond. eg, as.table=F, index.cond=list(c(3,2,1))
-#' @return a lattice plot
 #' @note specify auto through param colors, shapes
+#' @return a lattice plot
 #' @export
 ez.scatterplot = function(df,cmd,lmline=TRUE,loess=FALSE,model=c('lm', 'lmrob', 'lmRob', 'rlm'),type=c('spartial','partial'),scale=FALSE,rp=TRUE,rp.x=0.025,rp.y=0.05,se=TRUE,layout=NULL,
     rp.size=14,line.width=3,point.size=14,x.axis.size=16,y.axis.size=16,x.lab.size=18,y.lab.size=18,title.size=20,legend.size=c(0,14),
