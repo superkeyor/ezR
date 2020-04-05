23d574077b9f862f1b8bb538004efbbe195c87c9
Jerry Zhu <jerryzhujian9@gmail.com>
2020-04-05 18:34:45 -0400

    update

--- R/os.R
+++ R/os.R
@@ -296,10 +296,48 @@ ez.updateself = function(force=F) {
 #' print the version of a package
 #' @description print the version of a package
 #' @param pkg package name in quotes, default is NULL
+#' @param all logical.  If \code{TRUE} all of the functions from the package 
+#' will be displayed regardless of whether they're exported or not.
+#' @param character.only logical. If \code{TRUE} the input is a variable 
+#' containing the package name.
 #' @return if pkg not provided, prints R version, installed packages/versions and etc
 #' @export
-ez.ver = function(pkg=NULL){
+ez.ver = function(pkg=NULL, all = FALSE, character.only = FALSE){
     if (!is.null(pkg)) {
+        # https://github.com/trinker/pacman/blob/master/R/p_functions.R
+        pacman_functions <- 
+        function (package = "base", all = FALSE, character.only = FALSE){
+
+            # if (!character.only & is.name(substitute(package))) {
+            #     package <- deparse(substitute(package))
+            # }
+            
+            
+            # # Shouldn't have to check for this with deparse
+            # # Guess it can't hurt though...
+            # if (identical(package, character(0))) {
+            #     package <- "base"
+            # }
+            
+            ## We should probably do some error checking 
+            ## TODO: Add some error checking and display custom
+            ##       message if user asks for a package that
+            ##       doesn't exist
+            ns <- loadNamespace(package)
+            
+            ## Should we mark non-exported functions with asterisks or something?
+            if(all){
+                packagefunctions <- ls(ns)
+            }else{
+                packagefunctions <- getNamespaceExports(ns)
+            }
+
+            datas <- suppressWarnings(utils::data(package = package)[["results"]][, 3])
+            packagefunctions <- packagefunctions[!packagefunctions %in% datas]
+
+            return(sort(packagefunctions))
+        }
+        print(pacman_functions(pkg,all,character.only))
         cat(sprintf("%s\n%s: %s", R.version.string, pkg, as.character(packageVersion(pkg))))
     } else {
         ip = installed.packages()[,c(1,3:4)]
